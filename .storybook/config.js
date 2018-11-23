92614ea06d4bcbf12a47cb5be388e448dfe51643
Ross Hill <ross@rosshill.ca>
2018-11-23 15:33:48 -0500

    Get storybook running

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,17 +1,17 @@
-import { configure, addDecorator } from '@storybook/react'
-import { withInfo } from '@storybook/addon-info'
+import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 
-import 'styles/fonts.css'
-import 'styles/colors.css'
+// import 'styles/fonts.css'
+// import 'styles/colors.css'
 
 const reqs = [
   require.context('components', true, /\.stories\.js$/),
   // require.context('../src/pages', true, /\.stories\.js$/),
   // require.context('../src/modals', true, /\.stories\.js$/)
-]
+];
 
-const loadStories = () => reqs.forEach(req => req.keys().forEach(filename => req(filename)))
+const loadStories = () => reqs.forEach(req => req.keys().forEach(filename => req(filename)));
 
-addDecorator((story, context) => withInfo('common info')(story)(context))
+addDecorator((story, context) => withInfo('common info')(story)(context));
 
-configure(loadStories, module)
+configure(loadStories, module);
